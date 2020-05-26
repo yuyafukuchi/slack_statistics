8eab8a780ad0289436b9eb157cdec8b5839906de
yuyafukuchi <>
2020-05-26 22:12:24 +0900

    replace deprecate method to new method

--- util.py
+++ util.py
@@ -1,9 +1,10 @@
-import configparser
-from collections import defaultdict
+import json
 import datetime
-from logging import basicConfig, getLogger, DEBUG, INFO
+import requests
+import configparser
 from slack import WebClient
-import csv
+from collections import defaultdict
+from logging import basicConfig, getLogger, DEBUG, INFO
 
 # --------------------------------------------------
 # loggingの設定
@@ -20,32 +21,37 @@ config_ini.read('config.ini', encoding='utf-8')
 # --------------------------------------------------
 # グローバル変数の定義
 # --------------------------------------------------
-SLACK_CHANNEL_NAMES = ['general','random']
+SLACK_CHANNEL_NAMES = config_ini['Config']['TARGET_CHANNELS'].split(',')
 TOKEN = config_ini['Auth']['TOKEN']
-MAX_MESSAGE = 500 #between 1 and 1000
-MAX_RANKING = 10
+MAX_MESSAGE = 100 #between 1 and 1000
+MAX_RANKING = int(config_ini['Config']['MAX_RANKING'])
+TIMEFORMAT = "%Y-%m-%d"
 
-timeformat = "%Y-%m-%d"
+print(SLACK_CHANNEL_NAMES)
+
 client = WebClient(TOKEN)
 
 def convert_userid_to_username(userid):
-    info = client.users_info(user=userid)
+    info = client.users_profile_get(user=userid)
     if not info['ok']:
         logger.error(info['error'])
         return
     else:
-        if info.get('user'):
-            user = info['user']
-            return user['real_name']
-        else:
-            raise Exception
+        user = info['profile']
+        return user['real_name']
 
+# TODO: - slackclientに置き換え
 def convert_channel_names_to_ids():
-    channels_list = client.channels_list()
+    # param = {'token': TOKEN,'types': "public_channel,private_channel"}
+    # channels_list = requests.get('https://slack.com/api/conversations.list',params=param)
+    # channels_list = channels_list.json()
+
+    channels_list = client.conversations_list(types="public_channel,private_channel")
+ 
     ids= {}
     if channels_list['ok']:
         for channel in channels_list['channels']:
-            if channel.get('name') and channel['name'] in SLACK_CHANNEL_NAMES:
+            if channel['name'] in SLACK_CHANNEL_NAMES:
                 ids[channel['id']] = channel['name']
         return ids
     else:
@@ -56,11 +62,11 @@ def fetch_history(dt_latest,dt_oldest):
     message_counts = defaultdict(int)
     reaction_counts = defaultdict(int)
 
-    logger.info(f'fetching messages from {dt_oldest.strftime(timeformat)} to {dt_latest.strftime(timeformat)}')
+    logger.info(f'fetching messages from {dt_oldest.strftime(TIMEFORMAT)} to {dt_latest.strftime(TIMEFORMAT)}')
 
     for channel_id,channel_name in SLACK_CHANNEL_IDS.items():
         logger.info(f'fetching messages from {channel_name}')
-        json_data = client.channels_history(
+        json_data = client.conversations_history(
             channel=channel_id, 
             count = MAX_MESSAGE,
             latest=str(dt_latest.timestamp()),
